9b17665502173a646b95d82f8b406082b3276299
Baku26 <bakunasa@go.stockton.edu>
2026-02-12 17:57:58 -0500

    Refactor AndrewHolder to use ArrayList
    Refactor AndrewHolder class to use ArrayList and simplify methods.

--- AndrewHolder.java
+++ AndrewHolder.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import javafx.scene.control.Tab;
 
 public class AndrewHolder {
-    private String bucket[];
+    
+    ArrayList<Integer> someList = new ArrayList<>();
+    private String[] bucket;
     private int size;
     
     public AndrewHolder() {
@@ -9,116 +13,102 @@ public class AndrewHolder {
     }
 
         public void addToBucket(String someUserInputString) {
-        ensureCapacity();
-        bucket[size] = someUserInputString;
-        size++;
-       }
-       
-    public void add(String someUserInputString, int position) {
-        if (position < 0 || position > size) {
-            return;
+          if ( size == bucket.length) {
+          bucket[size] = someUserInputString;
+          doubleBucketSize();
+          size++;
         }
-        ensureCapacity();
-        
-        for(int i = size; i > position; i--) {
-            bucket[i] = bucket[i-1];
-            
-           }
-           bucket[position] = someUserInputString;
-           size++;
-        }
-        public void remove(int position) {
-            if (position < 0 || position >= size) {
-                return;
-            }
-            for(int i = position; i < size - 1; i++) {
-                bucket[i] = bucket[i+1];
-            }
-            bucket[size - 1] = null;
-            size--;
-        }
-          public int size() {
-        return size;
     }
-    public String get(int index) {
-          if( index >= 0 && index < size) {
-              return bucket[index];
-          }
-          return null;
+        public int size() {
+            return size;
         }
-        public void print() {
-                System.out.println("[");
-                for(int i = 0; i < size; i++) {
-                    System.out.print(bucket[i]);
-                    if (i < size - 1) {
-                        System.out.print(",");
-                    }
-            }
-            System.out.print("]");
-        }
-        public void clear() {
-            bucket = new String[10];
-            size = 0;
-        }
-        private void ensureCapacity() {
-            if (size == bucket.length) {
-                String[] newBucket = new String[bucket.length * 2];
-                for(int i = 0; i< bucket.length; i++){
-                    newBucket[i] = bucket[i];
-                }
-                bucket = newBucket;
-                }
-            }
-            public void addToEnd(String someUserInputString) {
-                ensureCapacity();
-                bucket[size] = someUserInputString;
-                size++;
-                
-            }
-            public AndrewHolder cloneClass() {
-             AndrewHolder copy = new AndrewHolder();
-             
-             copy.bucket = new String[this.bucket.length];
-             
-             for ( int i = 0; i < this.size; i++) {
-                 copy.bucket[i] = this.bucket[i];
-             }
-             copy.size = this.size;
-             return copy;
-              }
-              public String[] getBucket() {
-                  return bucket;
-              }
-              public void replace(String someUserInputString, int position) {
-                  if( position < 0 || position >= size) {
-                      return;
-                  }
-                  bucket[position] = someUserInputString;
-              }
-              public boolean find(String valueToFind) {
-                  for(int i = 0; i < size; i++) {
-                      if (bucket[i] != null && bucket[i].equals(valueToFind)) {
-                          return true;
-                      }
-                  }
-                  return false;
-                }
-                public int findCount(String valueToCount) {
-                    int count = 0;
-                    
-                    for (int i = 0; i < size; i++) {
-                    if (bucket[i] != null && bucket[i].equals(valueToCount)) {
-                        count++;
-                    }
-                
-                else if (bucket[i] == null && valueToCount == null) {
-                    count++;
-                 }
-                }
-                return count;
-            
-            }
+        public void getStringAtIndex(int index) {
+            System.out.println(bucket[index]);
     
-            
-        }
+    }
+    public void clearBucket() {
+        bucket = new String[10];
+        size = 0;
+    }
+    public void printAll() {
+        for (int i = 0; i < bucket.length; i++) {
+        System.out.println("[" + bucket[i] + "]");
+    }
+}
+     public void addToIndex( int index, String someUserInputString) {
+         if (size == bucket.length) {
+            doubleBucketSize();
+          bucket[index] = someUserInputString;
+             
+         }
+        } 
+         public void replaceAtIndex(int index, String someUserInputString) {
+           if ( size == bucket.length) {
+               doubleBucketSize();
+               bucket[index] = someUserInputString;
+           }
+         }
+    
+     public void find(String someUserInputString) {
+         for ( int i = 0; i < size - 1; i++) {
+             if(bucket[i].equalsIgnoreCase(someUserInputString)) {
+                 System.out.println(someUserInputString + "this is in the array");
+             }
+         }
+     }
+    public void findCount(String someUserInputString) {
+        int count = 0;
+        for ( int i = 0; i < size - 1; i++) {
+            if (bucket[i].equalsIgnoreCase(someUserInputString)) {
+                count++;
+                
+            }
+    }
+    System.out.println(someUserInputString + "has been found in the array " + count + "times");
+     
+}
+public void removeAtIndex( int index) {
+    bucket[index] = null;
+    for ( int i = index; i < size - 1; i++) {
+        bucket[i] = bucket[i + 1];
+    }
+    bucket[size - 1] = null;
+    size--;
+}
+public void doubleBucketSize() {
+ String[] bucket2 = new String[bucket.length * 2];
+ for(int i = 0; i < bucket.length; i++) {
+     bucket2[i] = bucket[i];
+ }
+ bucket = bucket2;
+}
+public void addToEnd(String someUserInputString ) {
+    if (size == bucket.length) {
+        doubleBucketSize();
+    }
+    bucket[size] = someUserInputString;
+    size++;
+}
+public AndrewHolder cloneClass() {
+   AndrewHolder andrewCopy = new AndrewHolder();
+   andrewCopy.bucket = new String[this.bucket.length];
+   for(int i = 0; i < size; i++) {
+       andrewCopy.bucket[i] = this.bucket[i];
+   }
+   andrewCopy.size = this.size;
+   return andrewCopy;
+}
+public String[] getBucket() {
+    String[] arrayValues = new String[size];
+    for(int i = 0; i < size; i++) {
+        arrayValues[i] = bucket[i];
+    }
+    return arrayValues;
+  }
+ // public void setObject(Tab objInput) {
+    
+  }
+
+
+            }
         
